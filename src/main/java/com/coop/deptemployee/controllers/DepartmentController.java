5f3c9666e1c10e300ab377befa1b3b01ccb54e7d
yashen97 <yashenraveesha97@gmail.com>
2021-08-11 10:18:49 -0700

    5th commit

--- src/main/java/com/coop/deptemployee/controllers/DepartmentController.java
+++ src/main/java/com/coop/deptemployee/controllers/DepartmentController.java
@@ -1,4 +1,51 @@
 package com.coop.deptemployee.controllers;
 
+import com.coop.deptemployee.models.Department;
+import com.coop.deptemployee.services.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/department")
 public class DepartmentController {
+
+    @Autowired
+    DepartmentService departmentService;
+
+    @PostMapping("/post")
+    public String postDepartment(@RequestBody Department department){
+        departmentService.addOrUpdate(department);
+        return "200 Ok";
+    }
+
+    @PostMapping("/postall")
+    public String postAllDepartments(@RequestBody List<Department> departments){
+        departmentService.addAllDepartments(departments);
+        return "200 Ok";
+    }
+
+    @GetMapping("/get")
+    public @ResponseBody List<Department> getDepartments(){
+        return departmentService.getAllDepartments();
+    }
+
+    @GetMapping("/getone/{id}")
+    public @ResponseBody Optional<Department> getOneDept(@PathVariable Long id){
+        return departmentService.getOneDepartment(id);
+    }
+
+    @PutMapping("/update")
+    public String updateDept(@RequestBody Department department){
+        departmentService.addOrUpdate(department);
+        return "204 Updated";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteDept(@PathVariable Long id){
+        departmentService.deleteDepartment(id);
+        return "204 Deleted";
+    }
 }
--- src/main/java/com/coop/deptemployee/services/DepartmentService.java
+++ src/main/java/com/coop/deptemployee/services/DepartmentService.java
@@ -1,8 +1,35 @@
 package com.coop.deptemployee.services;
 
+import com.coop.deptemployee.models.Department;
+import com.coop.deptemployee.repositories.DeptRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DepartmentService {
-//TODO:continue to the department part
+
+    @Autowired
+    DeptRepo deptRepo;
+
+    public Department addOrUpdate(Department department){
+       return deptRepo.save(department);
+    }
+
+    public List<Department> addAllDepartments(List<Department> departments){
+        return deptRepo.saveAll(departments);
+    }
+
+    public List<Department> getAllDepartments(){
+        return deptRepo.findAll();
+    }
+
+    public Optional<Department> getOneDepartment(Long id){
+        return deptRepo.findById(id);
+    }
+
+    public void deleteDepartment(Long id){
+        deptRepo.deleteById(id);
+    }
 }
